a9d98ebc4de17d78b8c6e92d357a3c3e2e633db2
gauta25 <gautamkanwar9@gmail.com>
2025-07-26 20:56:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas
 
 # Write directly to the app
 st.title("🥤Customize Your Smoothie!🥤")
@@ -20,11 +21,17 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 st.dataframe(data = my_dataframe, use_container_width = True)
 st.stop()
 
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
 ingredients_list = st.multiselect('Choose up to 5 ingridients', max_selections=5, options = my_dataframe)
 
 if ingredients_list:
     ingredients_string = ''
     for fruit in ingredients_list:
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit + ' Nutrition Information')
         ingredients_string+=fruit + ' '
         smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/all")
